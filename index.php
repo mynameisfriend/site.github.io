550abcbe4fbf47951badb0eedbc048daeb855d75
mynameisfriend <36600522+mynameisfriend@users.noreply.github.com>
2018-02-18 21:21:13 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <head>
 </head>
 <body>
-<?php $servername = "db4free.net"; $username = "artemsquery"; $password = "qwaszxer"; // Create connection 
-$cn = mysqli_connect($servername, $username, $password); // Check connection 
-if ($cn->connect_error) { echo "Connection failed : " . $cn->connect_error; } echo "Connected successfully"; ?> 
+
+<?php 
+$servername = "localhost"; $username = "id4790032_artemiy"; 
+$password = "qwaszxer"; // Create connection 
+$cn = new mysqli($servername, $username, $password); // Check connection 
+if ($cn->connect_error) { echo "Connection failed : " . $cn->connect_error; } ?> 
+
 </body>
 </html>
